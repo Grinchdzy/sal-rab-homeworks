3cd4fd1821c6bc138a0471a2297dc134ba767e9b
Grinch <grigoriidzy@mail.ru>
2022-02-19 12:50:51 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,20 +38,25 @@ function sendRequest(name, phone, address, goods, sum) {
     
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
            // data.goods[i].title = goods[i].title;
           //  data.goods[i].count = goods[i].count;
-          data.goods.push(goods[i].title);
+          let goods1 ={
+              title: goods[i].title ,
+              count: goods[i].count
+          }
+        data.goods.push(goods1);
+        //data.goods[i] = goods1
     }
  
     data.client = name + ' ' + phone;
 
-    data.order.address ='ул. 'address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    data.order.address ='ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
     data.order.sum = sum;
 
    
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
